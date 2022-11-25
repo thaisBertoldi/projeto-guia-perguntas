acd66e2f665862547fb700b77e9c94c7bb5ac2ca
Thais B <thais_brt@hotmail.com>
2022-11-25 11:14:37 -0300

    feat: instalado e configurado lib body-parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const appExpress = express();
+const bodyParser = require("body-parser");
 
 appExpress.set("view engine", "ejs");
 appExpress.use(express.static('public'));
+
+appExpress.use(bodyParser.urlencoded({extended: false}));
+appExpress.use(bodyParser.json());
 
 appExpress.get("/", (req, res) => {
   res.render("index", {});
@@ -10,7 +14,12 @@ appExpress.get("/", (req, res) => {
 
 appExpress.get("/perguntar", (req, res) => {
     res.render("perguntar");
-  });
+});
+
+appExpress.post("/salvarpergunta", (req, res) => {
+    const titulo = req.body.titulo;
+    const descricao = req.body.descricao;
+});
 
 appExpress.listen(8181, () => {
   console.log("App rodando");
