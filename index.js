5cd167e27bff6715477bca813bc7863f33a89fed
Thais B <thais_brt@hotmail.com>
2022-11-25 15:10:10 -0300

    feat: instalacao sequelize, criacao pagina pergunta

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const appExpress = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const modelPergunta = require("./database/Pergunta");
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão ok');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 appExpress.set("view engine", "ejs");
 appExpress.use(express.static('public'));
@@ -9,7 +20,13 @@ appExpress.use(bodyParser.urlencoded({extended: false}));
 appExpress.use(bodyParser.json());
 
 appExpress.get("/", (req, res) => {
-  res.render("index", {});
+    modelPergunta.findAll({ raw: true, order: [
+        ['id', 'DESC'] // ASC = crescente || DESC = decrescente
+    ]}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });
 });
 
 appExpress.get("/perguntar", (req, res) => {
@@ -19,7 +36,28 @@ appExpress.get("/perguntar", (req, res) => {
 appExpress.post("/salvarpergunta", (req, res) => {
     const titulo = req.body.titulo;
     const descricao = req.body.descricao;
+    modelPergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
 });
+
+appExpress.get("/pergunta/:id", (req, res) => {
+    const id = req.params.id;
+    modelPergunta.findOne({
+        where: { id: id }
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
+        }else {
+            res.redirect("/");
+        }
+    })
+})
 
 appExpress.listen(8181, () => {
   console.log("App rodando");
